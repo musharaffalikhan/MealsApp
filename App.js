fff17f8e5db6b1338933e78b7c4c7277f9325873
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-08-28 11:33:25 +0530

    Added background color and passing data dynamically to the components

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import { StyleSheet } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverview from "./screens/MealsOverview";
+import MealDetailsScreen from "./screens/MealDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-        <Stack.Screen name="MealsOverview" component={MealsOverview}/>
-      </Stack.Navigator>  
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle={"light-content"} />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#351401",
+            },
+            headerTintColor: "white",
+            contentStyle: {
+              backgroundColor: "#3f2f25",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen name="MealsOverview" component={MealsOverview} />
+          <Stack.Screen name="MealDetail" component={MealDetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
